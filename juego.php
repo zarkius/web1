661b858b6e1d727ce837b4e17fcf6d6efcde30fa
diego <63115070+zarkius@users.noreply.github.com>
2025-03-23 03:08:07 +0100

    modified:   juego.php

--- juego.php
+++ juego.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="styles1.css">
     <title>World1</title>
 </head>
-<body  style="background-color: red;">
+<body>
 <?php
 $servername = "localhost";
 $username = "zarkius";
@@ -23,17 +23,41 @@ if ($conn->connect_error) {
 }
     # echo "Connected successfully"; COMENTAR EN PRODUCCIÓN
 
+?>
+
+<div id="data1" style="border: 1px,solid,red; width:260px; position: relative; left: 35%; top:15px; font-size: 1.5rem;">
+<?php
+
 $sql = "SELECT * FROM SaludDelMundo LIMIT 1";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of first row
     $row = $result->fetch_assoc();
-    echo "La Esperanza esta en : " . $row["medidorDeEsperanza"]; // Reemplaza "nombre_de_la_columna" con el nombre de la columna que deseas mostrar
+    echo 'La Esperanza esta en : ' . $row["medidorDeEsperanza"]; // Reemplaza "medidorDeEsperanza" con el nombre de la columna que deseas mostrar
 } else {
     echo "0 resultados";
 }
-?>  
-    <p>Hola Guerrero!</p>
+?>
+</div>
+
+<div id="data2" style="position: relative; border: 1px, solid,red; width:280px; top:50px; left: 35%; font-size: 1.2rem;">
+
+
+
+<?php
+$sql = "SELECT user FROM Usuarios WHERE id = 1"; // Ajusta la consulta según sea necesario
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of first row
+    $row = $result->fetch_assoc();
+    $nombre = $row["user"]; // Reemplaza "nombre" con el nombre de la columna que deseas mostrar
+    echo 'Hola ' . $nombre . ' Bienvenido a tu pueblo';
+} else {
+    echo "Usuario no encontrado";
+}
+?>
+</div>
 </body>
 </html>
